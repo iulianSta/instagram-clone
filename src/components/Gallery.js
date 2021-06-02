547f82357562995ddcda60cff477ac09d7990f5f
iulianstan <iulian1226@gmail.com>
2021-06-02 15:02:27 +0200

    src/components/Gallery/ Gallery update

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,20 @@
 // Gallery function
 
 const Gallery = () => {
-  return <div>Gallery section</div>;
+  return (
+    <div>
+      <div>
+        <span>POSTS</span>
+        <span>REELS</span>
+        <span>IGTV</span>
+        <span>TAGGED</span>
+      </div>
+      <div>
+        <div>3 Images</div>
+        <div>Another 3 Images</div>
+      </div>
+    </div>
+  );
 };
 
 // Export Gallery
