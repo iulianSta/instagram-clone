e9e10a239978e8df35b953810e5db38254afd64c
iulianstan <iulian1226@gmail.com>
2021-06-02 14:07:25 +0200

    src/components/ Created Nav function

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+// Nav function
+
+const Nav = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>I am a nav list</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+// Export default Nav
+export default Nav;
