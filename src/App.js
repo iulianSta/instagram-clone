2a20867046f2fd1d5f181fa1ce6f5240b612c7f5
iulianstan <iulian1226@gmail.com>
2021-06-02 14:14:52 +0200

    src/App/ Import Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 // Import Nav
 import Nav from "./components/Nav";
+// Import Header
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Header />
     </div>
   );
 }
