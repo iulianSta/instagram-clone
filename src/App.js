3821e71568dbdd3e7464bfdd3277b6edf4b6f2f1
iulianstan <iulian1226@gmail.com>
2021-06-07 14:56:43 +0200

    src/App.js/ Imported Data from data.json

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Nav from "./components/Nav";
 import Header from "./components/Header";
 // Import Gallery
 import Gallery from "./components/Gallery";
+// Import Data
+import Data from "./data.json";
+console.log(Data);
 
 function App() {
   return (
