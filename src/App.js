a2d1ba0fecf72324fc694f9baf8357e5980c62d2
iulianstan <iulian1226@gmail.com>
2021-07-14 15:07:47 +0200

    Reordering App

--- src/App.js
+++ src/App.js
@@ -1,62 +1,51 @@
 // Import React from "react"
 import React from "react";
 
-// Import Nav
+// Import area
 import Nav from "./components/Nav";
-
-// Import Header
 import Header from "./components/Header";
-
-// Import Gallery
 import Gallery from "./components/Gallery";
 
 // Import Data
 import Data from "./data.json";
+import Images from "./images.json";
 
 // Import images
-import Avatar from "./img/i-love-img.jpg";
-import Profile from "./img/jw-profile-img.jpg";
-import Ways from "./img/100ways.jpg";
-import Bullet from "./img/bullet-to-the-heart.jpg";
-import Dway from "./img/dway.jpg";
-import Fendiman from "./img/fendiman.jpg";
-import Oxygen from "./img/oxygen.jpg";
-import Papillon from "./img/papillon.jpg";
+// import Avatar from "./img/i-love-img.jpg";
+// import Profile from "./img/jw-profile-img.jpg";
+// import Ways from "./img/100ways.jpg";
+// import Bullet from "./img/bullet-to-the-heart.jpg";
+// import Dway from "./img/dway.jpg";
+// import Fendiman from "./img/fendiman.jpg";
+// import Oxygen from "./img/oxygen.jpg";
+// import Papillon from "./img/papillon.jpg";
 
 // App function
 function App() {
   const post = Data.map((obj) => {
-    const {
-      id,
-      owner,
-      profileImg,
-      posts,
-      followers,
-      following,
-      team,
-      website,
-    } = obj;
+    const { id, owner, posts, followers, following, team, website } = obj;
 
     return (
       <div className="post" key={id}>
-        <Nav Avatar={Avatar} />
+        <Nav Images={Images} />
+        {/* <Nav avatar={avatar} /> */}
         <Header
           owner={owner}
-          profileImg={profileImg}
           posts={posts}
           followers={followers}
           following={following}
           team={team}
           website={website}
-          Profile={Profile}
+          profile={profile}
         />
         <Gallery
-          Ways={Ways}
-          Bullet={Bullet}
-          Dway={Dway}
-          Fendiman={Fendiman}
-          Oxygen={Oxygen}
-          Papillon={Papillon}
+          Images={Images}
+          // ways={ways}
+          // bullet={bullet}
+          // dway={dway}
+          // fendiman={fendiman}
+          // oxygen={oxygen}
+          // papillon={papillon}
         />
       </div>
     );
